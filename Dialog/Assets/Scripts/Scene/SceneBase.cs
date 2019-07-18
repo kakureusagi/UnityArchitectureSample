1ef672965479c536947cddd3218f7cc88e663333
kakureusagi <kakureusagi@gmail.com>
2019-07-18 10:51:47 +0900

    文字列定数化

--- Dialog/Assets/Scripts/Scene/SceneBase.cs
+++ Dialog/Assets/Scripts/Scene/SceneBase.cs
@@ -6,13 +6,15 @@ namespace MvpSample {
 
 	public class SceneBase : MonoBehaviour {
 
+		static readonly string SystemSceneName = "System";
+
 		protected IDialogFactory dialogFactory;
 		
 
 		protected virtual async UniTask Start() {
-			await SceneManager.LoadSceneAsync("System", LoadSceneMode.Additive);
+			await SceneManager.LoadSceneAsync(SystemSceneName, LoadSceneMode.Additive);
 
-			var rootGameObjects = SceneManager.GetSceneByName("System").GetRootGameObjects();
+			var rootGameObjects = SceneManager.GetSceneByName(SystemSceneName).GetRootGameObjects();
 			foreach (GameObject rootGameObject in rootGameObjects) {
 				dialogFactory = rootGameObject.GetComponentInChildren<IDialogFactory>();
 				if (dialogFactory != null) {
